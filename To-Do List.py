c377cb323049d1b6029c70bb83f1bb6e498df7be
Andrey-Peregudov <anmriya12@gmail.com>
2024-09-26 22:17:30 +0300

    Add file

---
+++ To-Do List.py
@@ -0,0 +1,7 @@
+#Функция создания задачи
+def create_task(title, description):
+    f = open(f'{title}.txt', 'w')
+    f.write(f'{description}')
+    f.close()
+#Вызов функции
+create_task(title="Задача", description="Утренняя пробежка")
